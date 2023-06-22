1a59575a41f31e7d3f055794120036c27daf1ad4
Ömer Ünal <mrfrknl3232@gmail.com>
2023-06-22 18:43:58 +0200

    2206/V5

--- src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
+++ src/test/java/test/C15_BaseUrlJsonPlaceHolder.java
@@ -2,6 +2,8 @@ package test;
 
 import baseUrl.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -31,17 +33,48 @@ public class C15_BaseUrlJsonPlaceHolder extends JsonPlaceHolderBaseUrl {
         // 2 - Expected Data hazirla
 
         // 3 - Response hazirla
-        Response response = given().spec(specJsonPlace).when().get("{/pp1}");
-        response.prettyPrint();
+        Response response = given().spec(specJsonPlace).when().get("/{pp1}");
+        //response.prettyPrint();
 
+        // 4 - Assertion
+        response.then().assertThat().statusCode(200).body("title", Matchers.hasSize(100));
 
     }
     @Test
     public void get02(){
+        // 2- https://jsonplaceholder.typicode.com/posts/44 endpointine bir GET request
+        //        gonderdigimizde donen response’un status code’unun 200 oldugunu ve “title”
+        //        degerinin “optio dolor molestias sit” oldugunu test edin
+
+        // 1 - Url hazirla
+        specJsonPlace.pathParams("pp1","posts","pp2",44);
+
+        // 2 - Expected Data hazirla
+
+        // 3 - Response hazirla
+        Response response = given().spec(specJsonPlace).when().get("/{pp1}/{pp2}");
+        response.prettyPrint();
+
+        // 4 - Assertion
+        response.then().assertThat().statusCode(200).body("title",Matchers.equalTo("optio dolor molestias sit"));
+
 
     }
     @Test
     public void delete01(){
+        // 3- https://jsonplaceholder.typicode.com/posts/50 endpointine bir DELETE request
+        //        gonderdigimizde donen response’un status code’unun 200 oldugunu ve response
+        //        body’sinin null oldugunu test edin
 
+        // 1 - Url hazirla
+        specJsonPlace.pathParams("pp1","posts","pp2",50);
+
+        // 2 - Expected Data hazirla
+
+        // 3 - Response hazirla
+        Response response = given().spec(specJsonPlace).when().delete("/{pp1}/{pp2}");
+
+        // 4 - Assertion
+        response.then().assertThat().statusCode(200).body("title", Matchers.nullValue());
     }
 }
---
+++ src/test/java/test/C17_BaseUrlHerokuappQueryParam.java
@@ -0,0 +1,76 @@
+package test;
+
+import baseUrl.HerokuAppBaseURL;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C17_BaseUrlHerokuappQueryParam extends HerokuAppBaseURL {
+    // Class icinde 3 Test metodu olusturun ve asagidaki testleri yapin
+
+    /*
+        1-  https://restful-booker.herokuapp.com/booking endpointine bir GET
+        request gonderdigimizde donen response’un status code’unun 200 oldugunu
+        ve Response’ta 1615 id'ye sahip bir booking oldugunu test edin
+     */
+
+     /*
+        2- https://restful-booker.herokuapp.com/booking endpointine gerekli
+        Query parametrelerini yazarak “firstname” degeri “Eric” olan rezervasyon
+        oldugunu test edecek bir GET request gonderdigimizde, donen response’un
+        status code’unun 200 oldugunu ve “Eric” ismine sahip en az bir booking oldugunu test edin
+    */
+
+    /*
+        3- https://restful-booker.herokuapp.com/booking endpointine gerekli Query
+         parametrelerini yazarak “firstname” degeri “Jim” ve “lastname” degeri
+         “Jackson” olan rezervasyon oldugunu test edecek bir GET request gonderdigimizde,
+         donen response’un status code’unun 200 oldugunu ve “Jim Jackson” ismine sahip
+         en az bir booking oldugunu test edin.
+    */
+    @Test
+    public void get01(){
+        // -  https://restful-booker.herokuapp.com/booking endpointine bir GET
+        //        request gonderdigimizde donen response’un status code’unun 200 oldugunu
+        //        ve Response’ta 1615 id'ye sahip bir booking oldugunu test edin
+
+        // 1 - Url hazirla
+
+        specHerokuApp.pathParam("pp1","booking");
+
+        // 2 - Expected Data
+
+        // 3 - Response'i kaydet
+        Response response = given().spec(specHerokuApp).when().get("/{pp1}");
+        response.prettyPrint();
+
+        // 4 - Assertion
+        response.then().assertThat().statusCode(200).body("bookingid",Matchers.hasItem(1615));
+
+    }
+    @Test
+    public void get02(){
+        // 2- https://restful-booker.herokuapp.com/booking endpointine gerekli
+        //        Query parametrelerini yazarak “firstname” degeri “Eric” olan rezervasyon
+        //        oldugunu test edecek bir GET request gonderdigimizde, donen response’un
+        //        status code’unun 200 oldugunu ve “Eric” ismine sahip en az bir booking oldugunu test edin
+
+        // 1 - Url hazirla
+        specHerokuApp.pathParam("pp1","booking").queryParam("firstname","Eric");
+        // 2 - Expected Data
+
+        // 3 - Response'i kaydet
+        Response response = given().spec(specHerokuApp).when().get("/{pp1}"); // burada query param'lari tekrar yazmaya gerek yok
+        response.prettyPrint();
+
+        // 4 - Assertion
+        //response.then().assertThat().statusCode(200).body("bookingid",Matchers.hasSize(2));
+
+    }
+    @Test
+    public void get03(){
+
+    }
+}
